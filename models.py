a659d20ed0fbcd4dbd8cca5c18fcea1d25020cdf
chaeyeongyoon <ui88g216@gmail.com>
2022-01-06 10:08:02 +0000

    v0.1

---
+++ models.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channel, out_chaennel,):
+        super(ResidualBlock, self).__init__()
+        
+class BottleNeck(nn.Module):
+    def __init__(self):
+        super(BottleNeck, self).__init__()
+
+class Resnet(nn.Module):
+    def __init__(self):
+        super(Resnet, self).__init__()
---
+++ train.py
@@ -0,0 +1,23 @@
+import argparse
+import torch
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+
+def train(opt):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    num_epoch, batch_size = opt.num_epoch, opt.batch_size
+    trainset = ImageFolder(root="../people_data/train")
+    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=False, num_workers=2)
+    # testset = ImageFolder(root="../people_data/test")
+    # testloader = DataLoader(trainset, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    # print(trainset.classes) [female, male]
+    # print(trainset.class_to_idx) female:0, male:1
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_epoch', type=int, default=100, help='the number of epochs')
+    parser.add_argument('--batch_size', type=int, default=4, help='batch size')
+
+    opt = parser.parse_args()
+    train(opt)
